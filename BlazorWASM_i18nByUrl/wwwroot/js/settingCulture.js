f95a2afe9d77c12ec237ddcd2bcf46f6144aa930
阿盲 <zxc9921327@gmail.com>
2023-12-18 09:17:03 +0800

    add: project

---
+++ BlazorWASM_i18nByUrl/wwwroot/js/settingCulture.js
@@ -0,0 +1,31 @@
+﻿/* loading時檢查lang參數內容 */
+window.onload = function () {
+    var langValue = getParameterByName("lang");
+    if (langValue.length > 0 && window.localStorage['BlazorCulture'] != langValue) {
+        window.localStorage['BlazorCulture'] = langValue;
+    }
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+window.blazorCulture = {
+    get: () => window.localStorage['BlazorCulture'],
+    set: (value) => window.localStorage['BlazorCulture'] = value
+};
+
+/*取得組合url用的lang字串*/
+function GetLangString() {
+    var langVaule = window.localStorage['BlazorCulture'];
+    var result = "";
+    if (langVaule.length > 0) {
+        result = "lang=" + langVaule;
+    }
+    return result;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# BlazorWASM_i18nByUrl
- 
+# Blazor WebAssembly透過URL設定多語系
+* Blazor Webassembly
+* .Net 6
+> 由另一個[repository](https://github.com/Airethz/Blazor-WASM_Localization)專案修改出來的產物
+> <br>透過lang參數設定語系，重整頁面後生效，其他頁面會因應語言設定在route加入對應參數。
+
+
+筆記：[Url設定多國語系 流程說明](https://hackmd.io/@Airethz/H1bLS1dWj)
